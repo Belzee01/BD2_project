b6775c38c466f286d3b53a5637828424785fa551
Kajetan Lipensky <kajetanlipensky@gmail.com>
2017-11-13 17:46:15 +0100

    added models Table and DB

--- SQLAccess/SQLAccess/model/DatabaseModel.cs
+++ SQLAccess/SQLAccess/model/DatabaseModel.cs
@@ -8,5 +8,26 @@ namespace SQLAccess
 {
     class DatabaseModel
     {
+        private String name;
+        private int databaseId;
+        private String createDate;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int DatabaseId
+        {
+            get { return databaseId; }
+            set { databaseId = value; }
+        }
+
+        public String CreateDate
+        {
+            get { return createDate; }
+            set { createDate = value; }
+        }
     }
 }
--- SQLAccess/SQLAccess/model/TableModel.cs
+++ SQLAccess/SQLAccess/model/TableModel.cs
@@ -8,5 +8,18 @@ namespace SQLAccess
 {
     class TableModel
     {
+        private String schema;
+        private String tableName;
+        private List<String> columnNames;
+        private String dataType;
+        private int maxLength;
+        private int precision;
+
+        public string Schema { get => schema; set => schema = value; }
+        public string TableName { get => tableName; set => tableName = value; }
+        public List<string> ColumnNames { get => columnNames; set => columnNames = value; }
+        public string DataType { get => dataType; set => dataType = value; }
+        public int MaxLength { get => maxLength; set => maxLength = value; }
+        public int Precision { get => precision; set => precision = value; }
     }
 }
